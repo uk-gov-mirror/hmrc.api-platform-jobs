2427889d19f044d4104ae1ded136bb686bcabc18
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-08-06 14:28:17 +0100

    APIS-8273 Update to latest version of organisation domain library (#104)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   private lazy val bootstrapVersion = "10.7.0"
   private lazy val hmrcMongoVersion = "2.13.0"
   private lazy val appDomainVersion = "1.1.0"
-  private lazy val orgDomainVersion = "1.4.0"
+  private lazy val orgDomainVersion = "1.5.0"
 
   private lazy val compileDeps = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-backend-play-30"                  % bootstrapVersion,
